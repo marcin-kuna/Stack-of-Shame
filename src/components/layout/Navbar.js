dac638b602bad9982b949a88c34d7aa1274d0881
Marcin Kuna <marcin.m.kuna@gmail.com>
2020-07-17 11:34:12 +0200

    Nav visual tweaks

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ToStack from './ToStack';
+import Logo from '../../img/logo.svg';
 
 const Navbar = () => {
     return (
         <nav className="navbar text-white mb-5">
             <Link to="/">
-                <div className="pseudo-logo">
-                    <span className="d-block text-white h2">S . . . </span>
-                    <span className="d-block text-white h2">O - - - </span>
-                    <span className="d-block text-white h2">S . . . </span>
-                </div>
+                <img src={Logo} alt="" className="logo"/>
             </Link>
             <span className="navbar-brand mb-0 h1 mx-auto">Stack of Shame</span>
         </nav>
